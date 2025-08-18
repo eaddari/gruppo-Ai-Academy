9fc7484186bb26b6fdbe38c5eb63a585dcdf5e2d
Giorgio Caddeo <Giorgio.Caddeo@it.ey.com>
2025-08-18 18:05:29 +0200

    style: fix formatting conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,39 +1,41 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os, sys
-sys.path.insert(0, os.path.abspath('../../src'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../../src"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'es2_classi_studenti'
-copyright = '2025, em_al_gio'
-author = 'em_al_gio'
-release = '0.1'
-language = 'Y'
+project = "es2_classi_studenti"
+copyright = "2025, em_al_gio"
+author = "em_al_gio"
+release = "0.1"
+language = "Y"
 
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'numpydoc',
-    'myst_parser',
-    'sphinx.ext.viewcode',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "numpydoc",
+    "myst_parser",
+    "sphinx.ext.viewcode",
 ]
 numpydoc_show_class_members = False
 
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
