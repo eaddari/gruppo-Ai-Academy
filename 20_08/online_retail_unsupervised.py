fd1d95366ab06eab59ce990efda3ecb6afc427cb
Emanuele Addari <emaddari@gmail.com>
2025-08-20 16:30:39 +0200

    <FEAT>(<online_retail_unsupervised>):data cleaning

--- 20_08/online_retail_unsupervised.py
+++ 20_08/online_retail_unsupervised.py
@@ -1,15 +1,34 @@
 import os
 
 import pandas as pd
+from sklearn.discriminant_analysis import StandardScaler
+
+
 
 DATASET_PATH = os.path.join(os.path.dirname(__file__), "dataset", "Online_Retail.csv")
 
+CLEANED_DATASET_PATH = os.path.join(os.path.dirname(__file__), "dataset", "Online_Retail_cleaned.csv")
+
 df = pd.read_csv(
     DATASET_PATH,
-    sep=",",  # separatore corretto
-    encoding="latin1",  # evita errori Unicode
+    sep=",",
+    encoding="latin1",
     quotechar='"',
-)  # gestisce le virgolette nei campi testo
+)
 
 print(df.head())
 print(df.info())
+
+
+df.dropna(subset=["CustomerID"], inplace=True)
+
+df = df[(df["Quantity"] > 0) & (df["Quantity"] < 13)]
+
+df = df[(df["UnitPrice"] >= 0.01) & (df["UnitPrice"] < 10)]
+
+scaler = StandardScaler()
+df[["Quantity", "UnitPrice"]] = scaler.fit_transform(df[["Quantity", "UnitPrice"]])
+
+df.drop(columns=["StockCode", "Description"], inplace=True)
+
+df.to_csv(CLEANED_DATASET_PATH, index=False)
