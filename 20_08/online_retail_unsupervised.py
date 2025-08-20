62531e7292a4b8a2e4fbd7df8a41a7827d1b9e36
Giorgio Caddeo <Giorgio.Caddeo@it.ey.com>
2025-08-20 15:55:35 +0200

    fix: adding DATASET_PATH constant instead of hardcoded file path

--- 20_08/online_retail_unsupervised.py
+++ 20_08/online_retail_unsupervised.py
@@ -1,9 +1,15 @@
+import os
+
 import pandas as pd
 
-df = pd.read_csv("20_08/dataset/Online_Retail.csv", 
-                 sep=",",            # separatore corretto
-                 encoding="latin1",  # evita errori Unicode
-                 quotechar='"')      # gestisce le virgolette nei campi testo
+DATASET_PATH = os.path.join(os.path.dirname(__file__), "dataset", "Online_Retail.csv")
+
+df = pd.read_csv(
+    DATASET_PATH,
+    sep=",",  # separatore corretto
+    encoding="latin1",  # evita errori Unicode
+    quotechar='"',
+)  # gestisce le virgolette nei campi testo
 
 print(df.head())
 print(df.info())
