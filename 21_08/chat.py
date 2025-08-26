8916cbc4ab440d6c14046e2c016458aaa130a7da
Emanuele Addari <emaddari@gmail.com>
2025-08-26 09:44:03 +0200

    sistemato codice

--- 21_08/chat.py
+++ 21_08/chat.py
@@ -50,10 +50,10 @@ def setup_azure_openai():
     load_dotenv(dotenv_path=".env")
 
     # Recupera le variabili di ambiente
-    api_key = os.getenv("AZURE_OPENAI_API_KEY")
-    endpoint = os.getenv("AZURE_OPENAI_ENDPOINT")
-    deployment_chat = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME_CHAT")
-    deployment_embedding = os.getenv("AZURE_OPENAI_DEPLOYMENT_NAME_EMBEDDING")
+    api_key = os.getenv("AZURE_INFERENCE_CREDENTIAL")
+    endpoint = os.getenv("AZURE_INFERENCE_ENDPOINT")
+    deployment_chat = os.getenv("AZURE_INFERENCE_CHAT_DEPLOYMENT_NAME")
+    deployment_embedding = os.getenv("AZURE_INFERENCE_DEPLOYMENT_NAME")
     api_version = os.getenv("AZURE_OPENAI_API_VERSION")
 
     if not all([api_key, endpoint, deployment_chat, deployment_embedding, api_version]):
